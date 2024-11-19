29271fbe3685c0355854655519db14873d15d479
Thomaskutty Reji <thomaskutty13.reji@gmail.com>
2024-11-20 01:56:02 +0530

    update

--- gemini/test.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-import numpy as np
-array = np.array([1,31,23])
-print('pandas version = ',pd.__version__)
-print('numpy version = ',np.__version__)
-
-print('array = ',array)
